36c0f1d56341c65ae178d0860bb4f8fa0572f480
Hasfarisha <hasfarisha@gmail.com>
2019-01-02 10:46:52 +0800

    coding socket programming dimasukkan

--- ForkTcpDump.c
+++ ForkTcpDump.c
@@ -17,6 +17,32 @@ void forks(void);
 void rest(void); 
 int main()
 {
+int sockfd;
+struct sockaddr_in serverAddr;
+
+int newSocket;
+struct sockaddr_in newAddr;
+
+socklen_t addr_size;
+char buffer[1024];
+
+sockfd=socket(PF_INET, SOCK_STREAM, 0);
+memset(&serverAddr, '\0', sizeof(serverAddr));
+
+serverAddr.sin_family=AF_INET;
+serverAddr.sin_port=htons(PORT);
+serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+listen(sockfd, 5);
+addr_size = sizeof(newAddr);
+
+newSocket=accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+
+strcpy(buffer, "Hello");
+send(newSocket, buffer, strlen(buffer), 0);
+
    int n, c = 2;
    int choice;
       
